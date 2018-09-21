02aa1c8ee6670d6d2358c49df829e0be8503587e
jordanraitt <jordanraitt@hotmail.com>
2018-09-21 12:40:02 +0100

    homework bar extensions

--- rooms.rb
+++ rooms.rb
@@ -1,12 +1,51 @@
 class Room
 
-attr_accessor :room_capacity, :tab
-attr_reader :song_list
+attr_accessor :tab
+attr_reader :song_list, :room_capacity
 
-  def initialize(song_list)
-    @room_capacity = 0
+  def initialize(room_capacity = [], song_list = [])
+    @room_capacity = room_capacity
     @tab = 0
     @song_list = song_list
   end
 
+  def add_guest(guest)
+    @room_capacity.push(guest)
+  end
+
+  def remove_guest(guest)
+    @room_capacity.delete(guest)
+  end
+
+  def get_guest_count
+    @room_capacity.length
+  end
+
+  def add_song(song)
+    @song_list.push(song)
+  end
+
+  def get_song_count
+    @song_list.length
+  end
+
+# def add_song()
+#
+# if
+#
+# end
+
+
+
+
+# def my _menu
+#   puts '1'
+#   puts
+#   puts
+#
+#   if response == '1'
+#     puts "enter name"
+#     passanger_name = gets.chomp
+#     flight_202.add_passanger(passanger.new(passanger_name))
+#   end
 end
--- specs/rooms_specs.rb
+++ specs/rooms_specs.rb
@@ -1,11 +1,69 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../rooms.rb")
+require_relative("../guests.rb")
+require_relative("../songs.rb")
 
 class RoomsTest < MiniTest::Test
 
-  def setup
+  def setup()
 
+    @song_01 = Song.new("Careless_whisper")
+    @song_02 = Song.new("Killer_queen")
+    @song_03 = Song.new("This_is_America")
+    @song_04 = Song.new("Shikantaza")
+    @song_05 = Song.new("Everybody_dance")
+    @song_06 = Song.new("Retro")
+    @song_07 = Song.new("Don't_leave_me_this_way")
+    @song_08 = Song.new("Trouble")
+    @song_09 = Song.new("The_world_(is_going_up_in_flames)")
+    @song_10 = Song.new("I_was_made_to_love_her")
+
+    @guest_01 = Guest.new("John", 15)
+    @guest_02 = Guest.new("Mark", 10)
+    @guest_03 = Guest.new("Peter", 11)
+    @guest_04 = Guest.new("James", 19)
+    @guest_05 = Guest.new("Andrew", 14)
+    @guest_06 = Guest.new("Philip", 16)
+    @guest_07 = Guest.new("Thomas", 16)
+    @guest_08 = Guest.new("Matt", 12)
+    @guest_09 = Guest.new("Simon", 14)
+    @guest_10 = Guest.new("Bart", 15)
+    @guest_11 = Guest.new("Luke", 10)
+    @guest_12 = Guest.new("Matthias", 17)
+    @guest_13 = Guest.new("Thaddaeus", 18)
+    @guest_14 = Guest.new("Jesus", 666)
+    @guest_15 = Guest.new("Judas", 13)
+
+    @room = Room.new([@guest_02],[@song_01])
   end
+
+  def test_has_guest
+    assert_equal([@guest_02] ,@room.room_capacity)
+  end
+
+  def test_has_song()
+    assert_equal([@song_01], @room.song_list)
+  end
+
+  def test_guest__add()
+    @room.add_guest(@guest_01)
+    assert_equal(2, @room.get_guest_count)
+  end
+
+  def test_guest__remove()
+    @room.add_guest(@guest_01)
+    @room.remove_guest(@guest_01)
+    assert_equal(1, @room.get_guest_count)
+  end
+
+  def test_guest__add()
+    @room.add_song(@song_02)
+    assert_equal(2, @room.get_song_count)
+  end
+
+
+
+
 
 end
